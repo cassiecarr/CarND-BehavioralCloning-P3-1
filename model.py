1c15575f066b9b7af75dda07309a5a788cccf8e6
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-14 15:21:50 -0400

    Add generator function

--- model.py
+++ model.py
@@ -1,44 +1,50 @@
 import csv
+import os
 import cv2
 import numpy as np
 
-lines = []
+samples = []
 with open('data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
-		lines.append(line)
+		samples.append(line)
 
-images = []
-measurements = []
-correction = 0.2
-for line in lines:
-	for i in range (3):
-		source_path = line[i]
-		filename = source_path.split('/')[-1]
-		current_path = 'data/IMG/' + filename
-		image = cv2.imread(current_path)
-		images.append(image)
-		measurement = float(line[3])
-		# print(i)
-		# print(filename)
-		# print(measurement)
-		# Left image
-		if i == 1 and measurement > 0.2:
-			measurement = measurement + correction
-		# Right image
-		if i == 2 and measurement > 0.2:
-			measurement = measurement - correction
-		# print(measurement)
-		measurements.append(measurement)
-		augmented_image = cv2.flip(image,1)
-		images.append(augmented_image)
-		augmented_measurement = measurement*-1.0
-		measurements.append(augmented_measurement)
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+import sklearn
 
-
-X_train = np.array(images)
-y_train = np.array(measurements)
+def generator(samples, batch_size=32):
+	num_samples = len(samples)
+    while 1: 
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+			images = []
+			measurements = []
+			correction = 0.2
+			for line in lines:
+				for i in range (3):
+					source_path = line[i]
+					filename = source_path.split('/')[-1]
+					current_path = 'data/IMG/' + filename
+					image = cv2.imread(current_path)
+					images.append(image)
+					measurement = float(line[3])
+					# Left image
+					if i == 1 and measurement > 0.2:
+						measurement = measurement + correction
+					# Right image
+					if i == 2 and measurement > 0.2:
+						measurement = measurement - correction
+					measurements.append(measurement)
+					augmented_image = cv2.flip(image,1)
+					images.append(augmented_image)
+					augmented_measurement = measurement*-1.0
+					measurements.append(augmented_measurement)
+			X_train = np.array(images)
+			y_train = np.array(measurements)
+			yield sklearn.utils.shuffle(X_train, y_train)
 
 from keras.models import Sequential
 from keras.layers import Lambda, Cropping2D
@@ -49,6 +55,9 @@ from keras.models import Model
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape = (160,320,3)))
@@ -65,7 +74,11 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam', metrics=['mse', 'accuracy'])
-history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2, verbose=1)
+history_object = model.fit_generator(train_generator, samples_per_epoch= /
+            		len(train_samples)*6, validation_data=validation_generator, /
+            		nb_val_samples=len(validation_samples)*6, nb_epoch=3, verbose=1)
+model.save('model.h5')
+import gc; gc.collect()
 
 ### print the keys contained in the history object
 print(history_object.history.keys())
@@ -78,7 +91,3 @@ plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.savefig('loss')
-
-model.save('model.h5')
-
-import gc; gc.collect()
