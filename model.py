cb8bed6edb8697a7e7693245a57175f854c336f8
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-14 09:29:50 -0400

    Add function to print accuracy

--- model.py
+++ model.py
@@ -37,7 +37,7 @@ model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(1))
 
-model.compile(loss = 'mse', optimizer = 'adam')
+model.compile(loss = 'mse', optimizer = 'adam', metrics=['mse', 'accuracy'])
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
 model.save('model.h5')
