7a8c983fa64ad77d65c1f5960ec2a424d2ebd670
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-14 11:12:08 -0400

    Change correction factor

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ images = []
 measurements = []
 correction = 0.2
 for line in lines:
-	for i in range (1):
+	for i in range (2):
 		source_path = line[i]
 		filename = source_path.split('/')[-1]
 		current_path = 'data/IMG/' + filename
@@ -21,10 +21,10 @@ for line in lines:
 		measurement = float(line[3])
 		# Left image
 		if i == 1:
-			measurement = measurement - correction
+			measurement = measurement + 0.5
 		# Right image
 		if i == 2:
-			measurement = measurement + correction
+			measurement = measurement - correction
 		measurements.append(measurement)
 		augmented_image = cv2.flip(image,1)
 		images.append(augmented_image)
