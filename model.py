978f8d36423b6b8bf4860885189b3c53ec547662
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-14 17:02:06 -0400

    Test to debug memory issue

--- model.py
+++ model.py
@@ -24,24 +24,24 @@ def generator(samples, batch_size=32):
 			measurements = []
 			correction = 0.2
 			for sample in samples:
-				for i in range (3):
+				for i in range (1):
 					source_path = sample[i]
 					filename = source_path.split('/')[-1]
 					current_path = 'data/IMG/' + filename
 					image = cv2.imread(current_path)
 					images.append(image)
-					measurement = float(sample[3])
-					# Left image
-					if i == 1 and measurement > 0.2:
-						measurement = measurement + correction
-					# Right image
-					if i == 2 and measurement > 0.2:
-						measurement = measurement - correction
+					# measurement = float(sample[3])
+					# # Left image
+					# if i == 1 and measurement > 0.2:
+					# 	measurement = measurement + correction
+					# # Right image
+					# if i == 2 and measurement > 0.2:
+					# 	measurement = measurement - correction
 					measurements.append(measurement)
-					augmented_image = cv2.flip(image,1)
-					images.append(augmented_image)
-					augmented_measurement = measurement*-1.0
-					measurements.append(augmented_measurement)
+					# augmented_image = cv2.flip(image,1)
+					# images.append(augmented_image)
+					# augmented_measurement = measurement*-1.0
+					# measurements.append(augmented_measurement)
 			X_train = np.array(images)
 			y_train = np.array(measurements)
 			yield sklearn.utils.shuffle(X_train, y_train)
