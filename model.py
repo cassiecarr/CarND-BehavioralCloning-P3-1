0aa7ab994fdeec813bc131962f64a2804dd1a5b8
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-15 13:13:43 -0400

    Add dropout to model

--- model.py
+++ model.py
@@ -50,7 +50,7 @@ def generator(samples, batch_size=32):
 
 from keras.models import Sequential
 from keras.layers import Lambda, Cropping2D
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.models import Model
@@ -61,6 +61,7 @@ import matplotlib.pyplot as plt
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
+# Appy NVIDIA Architecture
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape = (160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
@@ -71,8 +72,11 @@ model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.5))
 model.add(Dense(50))
+model.add(Dropout(0.5))
 model.add(Dense(10))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam', metrics=['mse', 'accuracy'])
