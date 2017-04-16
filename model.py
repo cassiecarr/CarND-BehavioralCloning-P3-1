eb15fe731a7ba359a78ad9592cf791baf0c0c155
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-16 19:22:56 -0400

    Add back NVIDIA model

--- model.py
+++ model.py
@@ -28,7 +28,7 @@ def generator(samples, batch_size=32):
 			measurements = []
 			correction = 0.2
 			for batch_sample in batch_samples:
-				for i in range (1):
+				for i in range (3):
 					source_path = batch_sample[i]
 					filename = source_path.split('/')[-1]
 					current_path = 'data/IMG/' + filename
@@ -43,10 +43,10 @@ def generator(samples, batch_size=32):
 					# Steering angle
 					measurement = float(batch_sample[3])
 					# If left image, adjust steering angle
-					if i == 1 and abs(measurement) > 0.3: 
+					if i == 1 and abs(measurement) > 0.2: 
 						measurement = measurement + correction
 					# If right image, adjust steering angle
-					if i == 2 and abs(measurement) > 0.3: 
+					if i == 2 and abs(measurement) > 0.2: 
 						measurement = measurement - correction
 					# Add steering angle
 					measurements.append(measurement)
@@ -78,22 +78,22 @@ validation_generator = generator(validation_samples, batch_size=32)
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160,320,3)))
 model.add(Cropping2D(cropping=((50,25), (0,0))))
-model.add(Convolution2D(6,5,5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5, activation="relu"))
-model.add(MaxPooling2D())
-# model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
-# model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
-# model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
-# model.add(Convolution2D(64,3,3, activation="relu"))
-# model.add(Convolution2D(64,3,3, activation="relu"))
-# model.add(Flatten())
-# model.add(Dense(100))
-# model.add(Dense(50))
-# model.add(Dense(10))
+# model.add(Convolution2D(6,5,5, activation="relu"))
+# model.add(MaxPooling2D())
+# model.add(Convolution2D(6,5,5, activation="relu"))
+# model.add(MaxPooling2D())
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+# model.add(Flatten())
+# model.add(Dense(120))
+# model.add(Dense(84))
 model.add(Dense(1))
 
 # Load weights
