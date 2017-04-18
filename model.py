b9d94434704fea2da42e207b9e641b47d847b75b
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-17 21:07:19 -0400

    Seperate pre-processing into utils, remove for 0 data

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import csv
 import os
 import cv2
 import numpy as np
+import utils
 
 # Read the csv file for image path
 samples = []
@@ -11,71 +12,25 @@ with open('data/driving_log.csv') as csvfile:
 	for line in reader:
 		samples.append(line)
 
-measurement_data = samples[3]
-
 # Seperate the data into training and validation sets
 import sklearn
 from sklearn.model_selection import train_test_split
 sklearn.utils.shuffle(samples)
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-# Define a generator to save images and add augmented images to the dataset
+# Define a generator to preprocess images, save images and 
+# add augmented images to the dataset
 def generator(samples, batch_size=32):
 	num_samples = len(samples)
 	while 1:
 		sklearn.utils.shuffle(samples)
 		for offset in range(0, num_samples, batch_size):
 			batch_samples = samples[offset:offset+batch_size]
-			images = []
-			measurements = []
-			correction = 0.2
-			count_zero_measurement = 0
-			removed = 0
-			for batch_sample in batch_samples:
-				# Remove every 4th zero
-				if abs(float(batch_sample[3])) < 0.03:
-					count_zero_measurement += 1
-				else:
-					count_zero_measurement = 0
-				if count_zero_measurement > 2:
-					removed += 1
-					count_zero_measurement = 0
-					continue
-				for i in range (3):
-					source_path = batch_sample[i]
-					filename = source_path.split('/')[-1]
-					current_path = 'data/IMG/' + filename
-					# Original image
-					image = cv2.imread(current_path)
-					# # Apply resize, crop, and apply histogram equalization to the image
-					image = cv2.resize(image, (235,118))
-					image = image[34:100, 17:217]
-					image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-					# Add image
-					images.append(image)
-					# Steering angle
-					measurement = float(batch_sample[3])
-					# If left image, adjust steering angle
-					if i == 1 and abs(measurement) > 0.2: 
-						measurement = measurement + correction
-					# If right image, adjust steering angle
-					if i == 2 and abs(measurement) > 0.2: 
-						measurement = measurement - correction
-					# Add steering angle
-					measurements.append(measurement)
-					# Add horizontally flipped image to dataset, adjust steering angle
-					augmented_image = np.fliplr(image)
-					images.append(augmented_image)
-					augmented_measurement = -measurement
-					measurements.append(augmented_measurement)
-					if (abs(measurement)) > 0.4:
-						images.append(image)
-						measurements.append(measurement)
-						images.append(augmented_image)
-						measurements.append(augmented_measurement)
+			
+			measurements, images = utils.preprocess(batch_samples)
+
 			X_train = np.array(images)
 			y_train = np.array(measurements)
-			# print(removed)
 			yield sklearn.utils.shuffle(X_train, y_train)
 
 # Import needed Keras functions
@@ -86,22 +41,14 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.models import Model
 from keras import optimizers
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
 
 # Call generator for training and validation data
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-# Appy NVIDIA Architecture for Keras model
+# Appy NVIDIA Architecture using Keras model
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (66,200,3)))
-# model.add(Cropping2D(cropping=((60,25), (0,0))))
-# model.add(Convolution2D(6,5,5, activation="relu"))
-# model.add(MaxPooling2D())
-# model.add(Convolution2D(6,5,5, activation="relu"))
-# model.add(MaxPooling2D())
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
 model.add(Dropout(0.2))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
@@ -116,13 +63,10 @@ model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
-# model.add(Flatten())
-# model.add(Dense(120))
-# model.add(Dense(84))
 model.add(Dense(1))
 
 # Load weights
-model.load_weights("weights.best.hdf5")
+# model.load_weights("weights.best.hdf5")
 
 adam = optimizers.Adam(lr=0.00001)
 model.compile(loss = 'mse', optimizer = adam, metrics=['mse', 'accuracy'])
@@ -144,10 +88,6 @@ import gc; gc.collect()
 
 # Print the keys contained in the history object
 print(history_object.history.keys())
-
-# Plot measurements
-plt.hist(measurement_data)
-plt.savefig('measurement histogram')
 
 # Plot the training and validation loss for each epoch
 plt.plot(history_object.history['loss'])
---
+++ utils.py
@@ -0,0 +1,76 @@
+import csv
+import os
+import cv2
+import numpy as np
+
+def preprocess(samples):
+	images = []
+	measurements = []
+	correction = 0.2
+	count_zero_measurement = 0
+	removed = 0
+
+	for batch_sample in samples:
+		# Remove every other zero
+		if abs(float(batch_sample[3])) < 0.05:
+			count_zero_measurement += 1
+		else:
+			count_zero_measurement = 0
+		if count_zero_measurement > 1:
+			removed += 1
+			count_zero_measurement = 0
+			continue
+
+		# Apply preprocessing to left, right and center images
+		for i in range (3):
+
+			# Read image
+			source_path = batch_sample[i]
+			filename = source_path.split('/')[-1]
+			current_path = 'data/IMG/' + filename
+			image = cv2.imread(current_path)
+
+			# Apply resize
+			image = cv2.resize(image, (235,118))
+
+			# Apply crop
+			image = image[34:100, 17:217]
+
+			# Convert to HSV color space
+			image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+			# Add image
+			images.append(image)
+
+			# Get steering angle
+			measurement = float(batch_sample[3])
+
+			# If left image, apply positive correction
+			if i == 1 and abs(measurement) > 0.2: 
+				measurement = measurement + correction
+
+			# If right image, apply negative correction
+			if i == 2 and abs(measurement) > 0.2: 
+				measurement = measurement - correction
+
+			# Add steering angle
+			measurements.append(measurement)
+
+			# Add horizontally flipped image to dataset, adjust steering angle
+			augmented_image = np.fliplr(image)
+			images.append(augmented_image)
+			augmented_measurement = -measurement
+			measurements.append(augmented_measurement)
+
+			# Add additional images when steering angle is greater than 0.4
+			if (abs(measurement)) > 0.4:
+				images.append(image)
+				measurements.append(measurement)
+				images.append(augmented_image)
+				measurements.append(augmented_measurement)
+			if (abs(measurement)) > 0.6:
+				images.append(image)
+				measurements.append(measurement)
+				images.append(augmented_image)
+				measurements.append(augmented_measurement)
+	return measurements, images
