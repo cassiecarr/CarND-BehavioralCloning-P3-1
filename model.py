ea7bebaaadcb9098e4a9695a8fb12f5e00ce8ce6
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-20 18:38:02 -0400

    Reduce epochs to 3

--- model.py
+++ model.py
@@ -79,7 +79,7 @@ model.compile(loss = 'mse', optimizer = adam, metrics=['mse'])
 
 # Generate the model
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*6, \
-	validation_data=validation_generator, nb_val_samples=len(validation_samples)*6, nb_epoch=5, \
+	validation_data=validation_generator, nb_val_samples=len(validation_samples)*6, nb_epoch=3, \
 	verbose=1)
 
 # Save the model
