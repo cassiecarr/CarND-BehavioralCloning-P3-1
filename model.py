72676560b0e23df9cd1af9e1686dcae0722b25e1
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-19 21:03:08 -0400

    Increase epochs, add dropout layer

--- model.py
+++ model.py
@@ -61,6 +61,7 @@ model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.2))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
@@ -79,7 +80,7 @@ model.compile(loss = 'mse', optimizer = adam, metrics=['mse'])
 
 # Generate the model
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*6, \
-	validation_data=validation_generator, nb_val_samples=len(validation_samples)*6, nb_epoch=5, \
+	validation_data=validation_generator, nb_val_samples=len(validation_samples)*6, nb_epoch=8, \
 	verbose=1)
 
 # Save the model
