487a9395293485eccd4b9cd58d9542cc1a4994c3
Cassandra Carr <cassandracarr@CassandrasMBP2.lan>
2017-04-20 21:02:02 -0400

    Add more curve images

--- utils.py
+++ utils.py
@@ -61,25 +61,21 @@ def preprocess(samples):
 			measurements.append(augmented_measurement)
 
 			# Add additional images when steering angle is greater than 0.4
-			if (abs(measurement)) > 0.4:
-				images.append(image)
-				measurements.append(measurement)
-				images.append(augmented_image)
-				measurements.append(augmented_measurement)
-			if (abs(measurement)) > 0.5:
-				images.append(image)
-				measurements.append(measurement)
-				images.append(augmented_image)
-				measurements.append(augmented_measurement)
-			if (abs(measurement)) > 0.6:
-				images.append(image)
-				measurements.append(measurement)
-				images.append(augmented_image)
-				measurements.append(augmented_measurement)
-			if (abs(measurement)) > 0.7:
-				images.append(image)
-				measurements.append(measurement)
-				images.append(augmented_image)
-				measurements.append(augmented_measurement)
+			for i in range(3):
+				if (abs(measurement)) > 0.4:
+					images.append(image)
+					measurements.append(measurement)
+					images.append(augmented_image)
+					measurements.append(augmented_measurement)
+				if (abs(measurement)) > 0.55:
+					images.append(image)
+					measurements.append(measurement)
+					images.append(augmented_image)
+					measurements.append(augmented_measurement)
+				if (abs(measurement)) > 0.7:
+					images.append(image)
+					measurements.append(measurement)
+					images.append(augmented_image)
+					measurements.append(augmented_measurement)
 
 	return measurements, images
